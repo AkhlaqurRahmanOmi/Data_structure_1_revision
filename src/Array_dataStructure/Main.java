7695350baf8695b633b7f9f73ce3c4f116fccbe2
AkhlaqurRahmanOmi <akhlaqur.rahman.omi@gmail.com>
2024-06-04 13:49:36 +0600

    adding some arrayList functionalities on the main

--- src/Array_dataStructure/Main.java
+++ src/Array_dataStructure/Main.java
@@ -1,8 +1,12 @@
 package Array_dataStructure;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Dynamic_array array = new Dynamic_array(2);
+
+        ArrayList <Integer> list = new ArrayList <>();
 
         array.insert(10);
         array.insert(20);
@@ -12,5 +16,15 @@ public class Main {
         array.removeAt(2);
         System.out.println(array.indexOf(40));
         array.print();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.remove(2);
+        list.lastIndexOf(list);
+        System.out.println(list.size());
+        System.out.println(list);
+        list.toArray();
     }
 }
